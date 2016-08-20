aade95ffd582fa8edd599c73f26c8fe1af41c066
Dmitriy Arkhipov <dmitriy.arkhipov.biz@mail.ru>
2016-08-20 23:42:22 +0800

    Initial Commit

---
+++ mediator_pattern/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mediator_pattern
+//
+//  Created by Dmitriy Arkhipov on 20.08.16.
+//  Copyright © 2016 Dmitriy Arkhipov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mediator_pattern/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mediator_pattern
+//
+//  Created by Dmitriy Arkhipov on 20.08.16.
+//  Copyright © 2016 Dmitriy Arkhipov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
